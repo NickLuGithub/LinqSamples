aa06392a7b41561e0c6494c7afa8af32ca21d870
NickLu <word4205@gmail.com>
2023-02-15 16:42:06 +0800

    新增 LinqSample004

---
+++ LinqSample005/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqSample005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
