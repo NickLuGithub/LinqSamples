d32dbdc7e3cbbc0666293b22a85e918facfbc04e
NickLu <word4205@gmail.com>
2023-02-15 16:17:12 +0800

    Distinct 每個值取一個 類似 Python set

--- LinqSample004/Program.cs
+++ LinqSample004/Program.cs
@@ -10,25 +10,31 @@ namespace LinqSample004
     {
         static void Main(string[] args)
         {
-            var list1 = new List<int> { 1, 2, 3, 4, 5, 6};
-            var list2 = new List<int> { 1, 3, 4, 7, 8, 9};
+            //var list1 = new List<int> { 1, 2, 3, 4, 5, 6};
+            //var list2 = new List<int> { 1, 3, 4, 7, 8, 9};
 
-            var union = list1.Union(list2);
-            Console.WriteLine($"聯集結果");
-            foreach (var item in union) Console.Write(item);
+            //var union = list1.Union(list2);
+            //Console.WriteLine($"聯集結果");
+            //foreach (var item in union) Console.Write(item);
 
-            var intersect = list1.Intersect(list2);
-            Console.WriteLine($"\n交集結果");
-            foreach (var item in intersect) Console.Write(item);
+            //var intersect = list1.Intersect(list2);
+            //Console.WriteLine($"\n交集結果");
+            //foreach (var item in intersect) Console.Write(item);
 
-            var aEXb = list1.Except(list2);
-            Console.WriteLine($"\nA 差集 B 結果");
-            foreach (var item in aEXb) Console.Write(item);
+            //var aEXb = list1.Except(list2);
+            //Console.WriteLine($"\nA 差集 B 結果");
+            //foreach (var item in aEXb) Console.Write(item);
 
-            var bEXa = list2.Except(list1);
-            Console.WriteLine($"\nB 差集 A 結果");
-            foreach (var item in bEXa) Console.Write(item);
+            //var bEXa = list2.Except(list1);
+            //Console.WriteLine($"\nB 差集 A 結果");
+            //foreach (var item in bEXa) Console.Write(item);
 
+            var list = new List<string> { "台北", "台北", "洛杉磯", "紐約", "紐約", "台北" };
+            var result = list.Distinct();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadLine();
         }
     }
